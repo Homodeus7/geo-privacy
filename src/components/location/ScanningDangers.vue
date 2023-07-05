76110c6a41848ba1cb45d44dd5aaaff585720a1c
homodeus7 <plum.bus@yahoo.com>
2023-07-05 20:03:32 +0300

    fix

--- src/components/location/ScanningDangers.vue
+++ src/components/location/ScanningDangers.vue
@@ -15,7 +15,7 @@
   <template>
     <v-overlay v-model="overlay" class="flex items-center justify-center">
       <div class="p-8 bg-[--black-middle] flex-col gap-4 relative rounded-md">
-        <div class="absolute right-7 top-7">
+        <div class="absolute right-5 top-5">
           <v-btn
             theme="dark"
             density="compact"
@@ -25,7 +25,75 @@
             @click="overlay = !overlay"
           ></v-btn>
         </div>
-        <div></div>
+        <v-text-field
+          theme="dark"
+          variant="solo"
+          placeholder="192.168.255.255"
+          class="min-w-[190px] pt-8"
+        ></v-text-field>
+        <div
+          class="flex item-center gap-2 mb-5 px-4 py-[10px] border-[1px] border-[--green]"
+        >
+          <v-btn
+            theme="dark"
+            density="compact"
+            icon="mdi-plus"
+            class="z-10"
+            color="#1d2926"
+            @click="active = !active"
+          ></v-btn>
+          <p class="lowercase font-medium">расширенный скан</p>
+        </div>
+        <div v-if="active" class="flex flex-col gap-4 pb-[16px]">
+          <v-text-field
+            theme="dark"
+            density="compact"
+            variant="solo"
+            label="Домен"
+            hide-details="auto"
+            class="min-w-[190px]"
+          ></v-text-field>
+          <v-text-field
+            theme="dark"
+            density="compact"
+            variant="solo"
+            label="IP"
+            hide-details="auto"
+            class="min-w-[190px]"
+          ></v-text-field>
+          <v-text-field
+            theme="dark"
+            density="compact"
+            variant="solo"
+            label="Ссылка"
+            hide-details="auto"
+            class="min-w-[190px]"
+          ></v-text-field>
+          <v-text-field
+            theme="dark"
+            density="compact"
+            variant="solo"
+            label="Логин"
+            hide-details="auto"
+            class="min-w-[190px]"
+          ></v-text-field>
+          <v-text-field
+            theme="dark"
+            density="compact"
+            variant="solo"
+            label="Пароль"
+            hide-details="auto"
+            class="min-w-[190px]"
+          ></v-text-field>
+        </div>
+        <v-btn
+          theme="dark"
+          size="large"
+          rounded="0"
+          class="text-none z-10"
+          color="#23A04D"
+          >Начать сканирование</v-btn
+        >
       </div>
     </v-overlay>
   </template>
@@ -92,6 +160,7 @@ import { reactive } from "vue";
 import { ref } from "vue";
 
 const overlay = ref(false);
+const active = ref(false);
 
 const scanningResults = reactive([
   {
