f82f460fdd432e72807c6c91f898c7c884ce6bae
homodeus7 <plum.bus@yahoo.com>
2023-07-25 12:15:11 +0300

    fix

--- src/components/home/ScanResults.vue
+++ src/components/home/ScanResults.vue
@@ -10,39 +10,56 @@
         <img src="@/assets/img/icon-left-arrow.svg" />
       </v-btn>
       <div class="flex flex-col gap-2">
-        <h1 class="font-medium text-3xl">Результаты сканирования</h1>
+        <h1 class="font-medium text-2xl">Результаты сканирования</h1>
         <div class="flex gap-2">
           <img src="@/assets/img/icon-refresh.svg" />
-          <p class="font-medium text-[--grey] lowercase">
+          <p class="text-sm font-medium text-[--grey] lowercase">
             последнее обновление
           </p>
-          <p class="font-medium">18.12.2023</p>
+          <p class="text-sm font-medium">18.12.2023</p>
         </div>
       </div>
     </div>
     <div class="flex gap-8 items-end">
       <div class="flex gap-2">
-        <p class="font-medium text-[--grey] lowercase">Сохранить отчет</p>
-        <v-btn theme="dark" size="small" class="tumb" color="#2D2E33">
-          PDF
-        </v-btn>
-        <v-btn theme="dark" size="small" class="tumb" color="#2D2E33">
-          XLS
-        </v-btn>
-        <v-btn theme="dark" size="small" class="tumb" color="#2D2E33">
-          HTML
+        <p class="text-sm font-medium text-[--grey] lowercase">
+          Сохранить отчет
+        </p>
+        <v-btn
+          v-for="button in buttons"
+          theme="dark"
+          size="small"
+          class="tumb"
+          color="#2D2E33"
+        >
+          {{ button.name }}
         </v-btn>
       </div>
       <div class="flex flex-col items-end gap-2">
         <div class="bg-[--blue-lighten] py-1 px-4 rounded">geoprivacy.com</div>
         <div class="flex gap-2">
           <img src="@/assets/img/icon-result-status.svg" />
-          <p class="font-medium text-[--grey] lowercase">статус сканирования</p>
-          <p class="font-medium lowercase">выполнено</p>
+          <p class="text-sm font-medium text-[--grey] lowercase">
+            статус сканирования
+          </p>
+          <p class="text-sm font-medium lowercase">выполнено</p>
         </div>
       </div>
     </div>
   </div>
 </template>
 
-<script setup></script>
+<script setup>
+import { ref } from "vue";
+
+const buttons = ref([
+  { name: "pdf", link: "#" },
+  { name: "xls", link: "#" },
+  { name: "html", link: "#" },
+]);
+</script>
+<style lang="scss" scoped>
+.tumb {
+  font-size: 16px;
+}
+</style>
